777fe1544d49b3ef8615b1c2223b86bf41a39aab
Eduardo Guilarducci <eduardo@guilarducci.com.br>
2024-01-14 12:18:19 -0300

    feat: add input data validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator";
+
 export class CreateUserDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   name: string;
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
   nickname: string;
 }
