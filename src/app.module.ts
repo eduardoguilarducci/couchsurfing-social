754f4a1c987ab897fdac704cd746a1c4b691beca
Eduardo Guilarducci <eduardo@guilarducci.com.br>
2024-01-14 11:42:08 -0300

    feat: add and configure nest-neo4j package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { Neo4jModule } from "nest-neo4j/dist";
 
 @Module({
-  imports: [],
+  imports: [
+    Neo4jModule.forRoot({
+      scheme: "neo4j",
+      host: "neo4j://localhost:7687",
+      port: 7474,
+      username: "neo4j",
+      password: "abc12345678",
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
